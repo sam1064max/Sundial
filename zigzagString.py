05876e282402f5edfb0f858525507ac6a8d269be
Sushant Shambharkar <sam1064max@gmail.com>
2022-05-28 11:03:58 +0530

    Update zigzagString.py

--- zigzagString.py
+++ zigzagString.py
@@ -1,22 +1,3 @@
-# The string "SUNDIALISHIRING" is written in a zigzag pattern on a given number of rows like this: 
-# (you may want to display this pattern in a fixed font for better legibility)
-
-# S  D  L  H  I
-# U  I  I  I  N
-# N  A  S  R  G
-
-# And then read line by line: "SDLHIUIIINNASRG"
-
-# Write the code that will take a string and make this conversion given a number of rows. 
-
-# Number of rows: 3
-# Input: SUNDIALISHIRING
-# Output: SDLHIUIIINNASRG
-
-#SUNDIALISHIRI
-
-#SDLHIUIIINNASRG
-
 from sys import stdout
 def solve(s, n):
     if n < 1 or type(n) == float : raise Exception("N should be positive integer")
@@ -26,12 +7,3 @@ def solve(s, n):
             stdout.write(s[n*col + row])
         if row < length % n:
             stdout.write(s[(length//n)*col + row])
-
-solve("SUNDIALISHIRING", 3)
-#S I S I
-#U A H N
-#N L I G
-#D I R
-
-#SISIUAHNNLIGDIR
-#print(solve("SUNDIALISHIRING", 3) == "SDLHIUIIINNASRG")
